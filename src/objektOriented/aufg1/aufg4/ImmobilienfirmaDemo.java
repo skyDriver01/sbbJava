4673067dbcbaa181b02de48686695b5d05d33909
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-10-26 08:34:32 +0200

    t

--- src/objektOriented/aufg1/aufg4/ImmobilienfirmaDemo.java
+++ src/objektOriented/aufg1/aufg4/ImmobilienfirmaDemo.java
@@ -3,7 +3,10 @@ package objektOriented.aufg1.aufg4;
 public class ImmobilienfirmaDemo {
     public static void main(String[] args) {
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> e411be9 (siu)
 =======
 >>>>>>> e411be9 (siu)
     }
