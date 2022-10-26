bab64d115a5f7ddfbbf8e2d61400ee3d2d14a3f9
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-10-26 15:33:53 +0200

    new files

---
+++ src/objektOriented/aufg1/aufg4/Contract.java
@@ -0,0 +1,9 @@
+package objektOriented.aufg1.aufg4;
+
+public class Contract {
+    private int ContractBeginning;
+    private int ContractEnd;
+    private int MonthlyPayment;
+    private String Gender;
+    private int Birthday;
+}
--- src/objektOriented/aufg1/aufg4/Immobilienfirma.java
+++ src/objektOriented/aufg1/aufg4/Immobilienfirma.java
@@ -1,8 +1,15 @@
 package objektOriented.aufg1.aufg4;
 
 public class Immobilienfirma {
-    private int number;
-
+    private int Number;
+    private String Adress;
+    private String Headquarters;
+    private String Employees;
+    private int Floor;
+    private int HouseNumber;
+    private String Janitor;
+    private String Tenant;
+    private String Houses;
 
 
 
