764d3e983060cdd2c115a74a5f2ea2a47da9ee3e
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-10-28 10:10:18 +0200

    2nd apartment list done 1st oen to do

--- src/objektOriented/aufg1/aufg4/ImmobilienfirmaDemo.java
+++ src/objektOriented/aufg1/aufg4/ImmobilienfirmaDemo.java
@@ -18,35 +18,37 @@ public class ImmobilienfirmaDemo {
     }
     public static void output(){
         firm();
-        property();
         contract();
     }
     public static void property(){
+        apartment();
         List<Property> propertyList = new ArrayList<>();
-        propertyList.add(new Property("","","",""));
-        propertyList.add(new Property("","","",""));
+        propertyList.add(new Property("Luka Nikolic","Rahul Gurung","Jamie Bläsi","Kornweg 71"));
+        propertyList.add(new Property("Yanir Gopal","Neil Ramseier","Sven Schmid","Ensingerstrasse 36"));
     }
     public static void apartment(){
         List<Apartment> apartmentList = new ArrayList<>();
         //Apartments for the first Property \/
-        apartmentList.add(new Apartment("",0,01,"","",""));
-        apartmentList.add(new Apartment("",0,01,"","",""));
-        apartmentList.add(new Apartment("",0,01,"","",""));
-        apartmentList.add(new Apartment("",0,01,"","",""));
-        apartmentList.add(new Apartment("",0,01,"","",""));
-        apartmentList.add(new Apartment("",0,01,"","",""));
+        apartmentList.add(new Apartment("Kornweg 71",1,11,"1'900","","Shervan Yacoub"));
+        apartmentList.add(new Apartment("Kornweg 71",1,12,"1'900","","Tobias Merino"));
+        apartmentList.add(new Apartment("Kornweg 71",1,13,"1'900","","Nick Bracher"));
+        apartmentList.add(new Apartment("Kornweg 71",2,21,"1'600","","Nik Wei"));
+        apartmentList.add(new Apartment("Kornweg 71",2,22,"1'600","","Manuel Möri"));
+        apartmentList.add(new Apartment("Kornweg 71",2,23,"1'600","","Jannik Pulfer"));
     //-------------------------------------------------------------------------------------------------------
         //Apartments for the second Property \/
-        apartmentList.add(new Apartment("",0,01,"","",""));
-        apartmentList.add(new Apartment("",0,01,"","",""));
-        apartmentList.add(new Apartment("",0,01,"","",""));
-        apartmentList.add(new Apartment("",0,01,"","",""));
-        apartmentList.add(new Apartment("",0,01,"","",""));
-        apartmentList.add(new Apartment("",0,01,"","",""));
+        apartmentList.add(new Apartment("Ensingerstrasse 36",1,11,"2'000","3, 1/2 Room apartment with a nice small Backyard","Nihat Yilmazer"));
+        apartmentList.add(new Apartment("Ensingerstrasse 36",1,12,"2'000","3, 1/2 Room apartment with a nice small Backyard","Michael Widmer"));
+        apartmentList.add(new Apartment("Ensingerstrasse 36",2,21,"2'250","4, 1/2 Room apartment with a nice cozy feeling","Joshua Scholze"));
+        apartmentList.add(new Apartment("Ensingerstrasse 36",2,22,"2'250","4, 1/2 Room apartment with a nice cozy feeling","Arthur Deierlein"));
+        apartmentList.add(new Apartment("Ensingerstrasse 36",3,31,"2'750","6 Room apartment with big terrace and nice View","Andrin Gurtner"));
+        apartmentList.add(new Apartment("Ensingerstrasse 36",3,32,"2'750","6 Room apartment with big terrace and nice View","Lars Ruch"));
     }
     public static void contract(){
+        apartment();
     }
     public static void firm() {
+        property();
         List<Immobilienfirma> tempList = new ArrayList<>();
         Stream<Immobilienfirma> temp = tempList.stream();
         tempList.add(new Immobilienfirma("Index 0 out of bounds", "No Entry"));
