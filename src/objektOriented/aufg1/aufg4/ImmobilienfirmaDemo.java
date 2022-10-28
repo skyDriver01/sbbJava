4bf71335c085f3d8ae9103c1e33364e95a6dd702
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-10-28 11:43:46 +0200

    List with empty houses

--- src/objektOriented/aufg1/aufg4/ImmobilienfirmaDemo.java
+++ src/objektOriented/aufg1/aufg4/ImmobilienfirmaDemo.java
@@ -19,9 +19,9 @@ public class ImmobilienfirmaDemo {
     }
     public static void output(){
         firm();
-        contract();
     }
     public static void property(){
+        Scanner scanner = new Scanner(System.in);
         List<Property> propertyList = new ArrayList<>();
         propertyList.add(new Property("Luka Nikolic","Rahul Gurung","Jamie Bläsi","Kornweg 71"));
         propertyList.add(new Property("Yanir Gopal","Neil Ramseier","Sven Schmid","Ensingerstrasse 36"));
@@ -33,23 +33,28 @@ public class ImmobilienfirmaDemo {
         apartmentList.add(new Apartment("Kornweg 71",1,11,"1'900Fr.","3 Room apartment with a good earthy feel to it","Shervan Yacoub"));
         apartmentList.add(new Apartment("Kornweg 71",1,12,"1'900Fr.","3 Room apartment with a good earthy feel to it","Tobias Merino"));
         apartmentList.add(new Apartment("Kornweg 71",1,13,"1'900Fr.","3 Room apartment with a good earthy feel to it","Nick Bracher"));
+        apartmentList.add(new Apartment("Kornweg 71",1,13,"1'900Fr.","3 Room apartment with a good earthy feel to it",""));
         apartmentList.add(new Apartment("Kornweg 71",2,21,"1'600Fr.","1, 1/2 Room apartment not very suitable to live in if you aren't used to small spaces (NO BALCONY)","Yanick Minder"));
         apartmentList.add(new Apartment("Kornweg 71",2,22,"1'600Fr.","1, 1/2 Room apartment not very suitable to live in if you aren't used to small spaces (NO BALCONY)","Manuel Möri"));
         apartmentList.add(new Apartment("Kornweg 71",2,23,"1'600Fr.","1, 1/2 Room apartment not very suitable to live in if you aren't used to small spaces (NO BALCONY)","Jannik Pulfer"));
-    //-------------------------------------------------------------------------------------------------------
+        apartmentList.add(new Apartment("Kornweg 71",2,24,"1'600Fr.","1, 1/2 Room apartment not very suitable to live in if you aren't used to small spaces (NO BALCONY)",""));
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         //Apartments for the second Property \/
         apartmentList.add(new Apartment("Ensingerstrasse 36",1,11,"2'000Fr.","3, 1/2 Room apartment with a nice small terrace","Nihat Yilmazer"));
         apartmentList.add(new Apartment("Ensingerstrasse 36",1,12,"2'000Fr.","3, 1/2 Room apartment with a nice small terrace","Michael Widmer"));
+        apartmentList.add(new Apartment("Ensingerstrasse 36",2,23,"2'250Fr.","4, 1/2 Room apartment with a nice cozy feeling",""));
         apartmentList.add(new Apartment("Ensingerstrasse 36",2,21,"2'250Fr.","4, 1/2 Room apartment with a nice cozy feeling","Joshua Scholze"));
         apartmentList.add(new Apartment("Ensingerstrasse 36",2,22,"2'250Fr.","4, 1/2 Room apartment with a nice cozy feeling","Arthur Deierlein"));
+        apartmentList.add(new Apartment("Ensingerstrasse 36",1,13,"2'000Fr.","3, 1/2 Room apartment with a nice small terrace",""));
         apartmentList.add(new Apartment("Ensingerstrasse 36",3,31,"2'750Fr.","6 Room apartment with big balcony and nice View","Andrin Gurtner"));
         apartmentList.add(new Apartment("Ensingerstrasse 36",3,32,"2'750Fr.","6 Room apartment with big balcony and nice View","Lars Ruch"));
+        apartmentList.add(new Apartment("Ensingerstrasse 36",3,33,"2'750Fr.","6 Room apartment with big balcony and nice View",""));
     }
     public static void contract(){
-        apartment();
+        property();
     }
     public static void firm() {
-        property();
+        contract();
         List<Immobilienfirma> tempList = new ArrayList<>();
         Stream<Immobilienfirma> temp = tempList.stream();
         tempList.add(new Immobilienfirma("Index 0 out of bounds", "No Entry"));
