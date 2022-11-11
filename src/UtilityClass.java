7c5d2891b73d451456fb79080d0912a16bd0320e
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-11-11 14:56:03 +0100

    utility start

--- src/UtilityClass.java
+++ src/UtilityClass.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class UtilityClass {
     public static void scanners() {
-
+        Scanner scanner = new Scanner(System.in);
+        String getScan = scanner.nextLine();
     }
 
     public static void switches() { //switch cases gemeint
+        Scanner scanner = new Scanner(System.in):
+        int aSwitch = Integer.parseInt(scanner.nextLine());
+        switch (aSwitch){
+            case 1 -> {}
+            case 2 -> {}
+            case 3 -> {}
+            case 4 -> {}
+            case 5 -> {}
 
+        }
     }
 
     public static void hashMaps() {
