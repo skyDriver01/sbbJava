f1d77df0dc565dc267d94ee826d8c4072ee05f6b
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-12-01 08:14:46 +0100

    no more scanners in stream site

--- src/objektOriented/aufg1/aufg3/StreamingSite.java
+++ src/objektOriented/aufg1/aufg3/StreamingSite.java
@@ -1,67 +1,63 @@
 package objektOriented.aufg1.aufg3;
 
+import objektOriented.aufg1.aufg2.InputIn;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class StreamingSite {
     public static void executeSite() {
         List<Person> tempList = new ArrayList<>();
         tempList.add(new Person("siu", "siu", "siu@", "1234 1234 1234 1234", "12/12", "012", true, Person.SubscriptionType.MONTHLY));
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, What would you like to do?");
         System.out.println("1: Create an Account");
         System.out.println("2: Log into your Account");
         System.out.println("3: Remove an account");
         System.out.println("4: Exit");
 
-        switchOptions(tempList, scanner);
+        switchOptions(tempList);
     }
 
-    private static void switchOptions(List<Person> tempList, Scanner scanner) {
+    private static void switchOptions(List<Person> tempList) {
         boolean exit = false;
         while (!exit) {
-            System.out.println("[Select your Number]");
-            int login = Integer.parseInt(scanner.nextLine());
+            int login = Integer.parseInt(InputIn.nextLineOut("[Select your Number]"));
             switch (login) {
-                case 1 -> exit = gatherUsersInformation(tempList, scanner);
+                case 1 -> exit = gatherUsersInformation(tempList);
 
-                case 2 -> exit = loginToExistingUser(tempList, scanner, exit);
+                case 2 -> exit = loginToExistingUser(tempList, exit);
 
-                case 3 -> deleteUser(tempList, scanner);
+                case 3 -> deleteUser(tempList);
                 case 4 -> exit = true;
             }
         }
     }
 
-    private static boolean gatherUsersInformation(List<Person> tempList, Scanner scanner) {
+    private static boolean gatherUsersInformation(List<Person> tempList) {
         boolean noSameUsernames = true;
-        System.out.println("Enter your username");
-        String username = scanner.nextLine();
+        String username = InputIn.nextLineOut("Enter your username");
         for (Person entry : tempList) {
             while (username.equals(entry.getUsername())) {
                 System.out.println("That Username is already in use please enter another one.");
-                username = scanner.nextLine();
+                username = InputIn.nextLine();
             }
 
         }
-        System.out.println("Enter a Password you will remember");
-        String password = scanner.nextLine();
-        String email = getUsersEmail(tempList, scanner);
+        String password = InputIn.nextLineOut("Enter a Password you will remember");
+        String email = getUsersEmail(tempList);
 
-        System.out.println("Enter your CreditCard Information");
-        String creditcard = scanner.nextLine();
-        creditcard = getCreditcardNumber(scanner, creditcard);
+        String creditcard = InputIn.nextLineOut("Enter your CreditCard Information");
+        creditcard = getCreditcardNumber(creditcard);
 
-        String creditcardExperation = getCreditcardExperation(scanner);
-        String creditcardSecurityNumber = getCreditcardSecurityNumber(scanner);
+        String creditcardExperation = getCreditcardExperation();
+        String creditcardSecurityNumber = getCreditcardSecurityNumber();
 
         System.out.println("Do you want to get a subscription to enjoy Ad-Free and unlimited Viewing? (Type true if you want a subscription or false if you don't want one");
         boolean subscription = true;
         Person.SubscriptionType subscriptionType = null;
-        if (subscription == Boolean.parseBoolean(scanner.nextLine())) {
+        if (subscription == Boolean.parseBoolean(InputIn.nextLine())) {
             System.out.println("What kind of subscription would you like the monthly subscription for 15Fr.(type: monthly) or the yearly subscription for 150fr.(type: yearly)");
-            subscriptionType = switch (scanner.nextLine().toUpperCase()) {
+            subscriptionType = switch (InputIn.nextLine().toUpperCase()) {
                 case "MONTHLY" -> Person.SubscriptionType.MONTHLY;
                 case "YEARLY" -> Person.SubscriptionType.YEARLY;
                 default -> null;
@@ -69,15 +65,14 @@ public class StreamingSite {
         }
         tempList.add(new Person(username, password, email, creditcard, creditcardExperation, creditcardSecurityNumber, subscription, subscriptionType));
 
-        streamSiteOn(tempList, scanner);
+        streamSiteOn(tempList);
         return noSameUsernames;
     }
 
-    private static String getUsersEmail(List<Person> tempList, Scanner scanner) {
+    private static String getUsersEmail(List<Person> tempList) {
         boolean noSameMail = true;
         boolean loop = true;
-        System.out.println("What is your E-mail address?");
-        String email = scanner.nextLine();
+        String email = InputIn.nextLineOut("What is your E-mail address?");
         while (noSameMail) {
             for (Person entry : tempList) {
                 while (loop) {
@@ -87,12 +82,12 @@ public class StreamingSite {
                             containsAnAet = true;
                         } else {
                             System.out.println("Your mail needs an @");
-                            email = scanner.nextLine();
+                            email = InputIn.nextLine();
                         }
                     }
                     if (email.equals(entry.getEmail())) {
                         System.out.println("Email is already in use try another one ");
-                        email = scanner.nextLine();
+                        email = InputIn.nextLine();
                     } else {
                         loop = false;
                     }
@@ -105,52 +100,47 @@ public class StreamingSite {
         return email;
     }
 
-    private static String getCreditcardNumber(Scanner scanner, String creditcard) {
+    private static String getCreditcardNumber(String creditcard) {
         while (!creditcard.matches("[0-9 ]{19}")) {
             System.out.println("Enter your Credit Card Information correctly pls.(Like : XXXX XXXX XXXX XXXX)");
-            creditcard = scanner.nextLine();
+            creditcard = InputIn.nextLine();
         }
         return creditcard;
     }
 
-    private static String getCreditcardExperation(Scanner scanner) {
-        System.out.println("Enter your CreditCards Experation Date pls. Keep in Mind to put a / between both numbers");
-        String creditcardExperation = scanner.nextLine();
+    private static String getCreditcardExperation() {
+        String creditcardExperation = InputIn.nextLineOut("Enter your CreditCards Experation Date pls. Keep in Mind to put a / between both numbers");
         while (!creditcardExperation.matches("[0-9 /]{5}")) {
             System.out.println("Enter your CreditCards Experation Date like this Pls(XX/XX)");
-            creditcardExperation = scanner.nextLine();
+            creditcardExperation = InputIn.nextLine();
         }
         return creditcardExperation;
     }
 
-    private static String getCreditcardSecurityNumber(Scanner scanner) {
-        System.out.println("Enter your CreditCards Security Number pls.");
-        String creditcardSecurityNumber = scanner.nextLine();
+    private static String getCreditcardSecurityNumber() {
+        String creditcardSecurityNumber = InputIn.nextLineOut("Enter your CreditCards Security Number pls.");
         while (!creditcardSecurityNumber.matches("[0-9]{3}")) {
             System.out.println("Enter your CreditCards Security Number like this pls.(XXX)");
-            creditcardSecurityNumber = scanner.nextLine();
+            creditcardSecurityNumber = InputIn.nextLine();
         }
         return creditcardSecurityNumber;
     }
 
-    private static boolean loginToExistingUser(List<Person> tempList, Scanner scanner, boolean exit) {
+    private static boolean loginToExistingUser(List<Person> tempList, boolean exit) {
         System.out.println("What account would you like to enter?");
         for (Person entry : tempList) {
             System.out.println(entry.getUsername() + ", ");
-        }
-        System.out.println("Type your username");
-        String desiredLogin = scanner.nextLine();
+        }                   //TODO scanner ändern zu InputIn util class
+        String desiredLogin = InputIn.nextLineOut("Type your username");
         for (Person entry : tempList) {
             if (desiredLogin.equals(entry.getUsername())) {
-                System.out.println("Type your password");
-                String desiredPassword = scanner.nextLine();
+                String desiredPassword = InputIn.nextLineOut("Type your password");
                 if (desiredPassword.equals(entry.getPassword())) {
                     System.out.println("Welcome back. Enjoy the movies");
                     exit = true;
                     boolean streamsiteOn = true;
                     System.out.println("Welcome to the Site. Please keep in mind while searching for movies to use proper grammar. Enjoy the movies");
-
-                    while (!streamsiteOn) {
+                    while (streamsiteOn) {
                         System.out.println("1: Search for a Movie");
                         System.out.println("2: Change your CreditCard Information");
                         System.out.println("3: View the amount of people with a subscription");         //TODO utility class für scanner so wie schon einmal erwähnt
@@ -158,17 +148,16 @@ public class StreamingSite {
                         System.out.println("5: Logout of your account");
                         System.out.println("[Select your Number]");
 
-                        int options = Integer.parseInt(scanner.nextLine());
+                        int options = Integer.parseInt(InputIn.nextLine());
                         switch (options) {
                             case 1 -> {
-                                System.out.println("Type 1: To search for a movie with its name. Type 2: To search for a movie with its genre.");
-                                int search = Integer.parseInt(scanner.nextLine());
+                                int search = Integer.parseInt(InputIn.nextLineOut("Type 1: To search for a movie with its name. Type 2: To search for a movie with its genre."));
                                 switch (search) {
                                     case 1 -> nameSearch();
                                     case 2 -> genreSearch();
                                 }
                             }
-                            case 2 -> changeCreditcard(scanner, tempList);
+                            case 2 -> changeCreditcard(tempList);
                             case 3 -> countUsersWithSubscriptions(tempList);
                             case 4 -> getCreditcardInfoFromMonthlyPaymentUsers(tempList);
                             case 5 -> executeSite();
@@ -192,7 +181,7 @@ public class StreamingSite {
         System.out.println(counter);
     }
 
-    private static void streamSiteOn(List<Person> tempList, Scanner scanner) {
+    private static void streamSiteOn(List<Person> tempList) {
         boolean streamsiteOn;
         streamsiteOn = false;
         System.out.println("Welcome to the Site. Please keep in mind while searching for movies to use proper grammar. Enjoy the movies");
@@ -204,17 +193,16 @@ public class StreamingSite {
             System.out.println("5: Logout of your account");
             System.out.println("[Select your Number]");
 
-            int options = Integer.parseInt(scanner.nextLine());
+            int options = Integer.parseInt(InputIn.nextLine());
             switch (options) {
                 case 1 -> {
-                    System.out.println("Type 1: To search for a movie with its name. Type 2: To search for a movie with its genre.");
-                    int search = Integer.parseInt(scanner.nextLine());
+                    int search = Integer.parseInt(InputIn.nextLineOut("Type 1: To search for a movie with its name. Type 2: To search for a movie with its genre."));
                     switch (search) {
                         case 1 -> nameSearch(); //TODO viewed und views machen
                         case 2 -> genreSearch();
                     }
                 }
-                case 2 -> changeCreditcard(scanner, tempList);
+                case 2 -> changeCreditcard(tempList);
                 case 3 -> countUsersWithSubscriptions(tempList);
                 case 4 -> getCreditcardInfoFromMonthlyPaymentUsers(tempList);
                 case 5 -> streamsiteOn = logOutOfTheStreamSite(tempList);
@@ -223,14 +211,13 @@ public class StreamingSite {
     }
 
     private static boolean logOutOfTheStreamSite(List<Person> tempList) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, What would you like to do?");
         System.out.println("1: Create an Account");
         System.out.println("2: Log into your Account");
         System.out.println("3: Remove an account");
         System.out.println("4: Exit");
 
-        switchOptions(tempList, scanner);
+        switchOptions(tempList);
         return true;
     }
 
@@ -242,16 +229,15 @@ public class StreamingSite {
         }
     }
 
-    private static void deleteUser(List<Person> tempList, Scanner scanner) {
+    private static void deleteUser(List<Person> tempList) {
         System.out.println("What Contact do you want to Delete?");
         for (Person entry : tempList) {
             System.out.println(entry.getUsername());
         }
-        String input = scanner.nextLine();
+        String input = InputIn.nextLine();
         for (int i = 0; i < tempList.size(); i++) {
             if (input.equalsIgnoreCase(tempList.get(i).getUsername())) {
-                System.out.println("Password pls:");
-                String pass = scanner.nextLine();
+                String pass = InputIn.nextLineOut("Password pls:");
                 if (pass.equalsIgnoreCase(tempList.get(i).getPassword())) {
                     tempList.remove(tempList.get(i));
                 }
@@ -259,14 +245,13 @@ public class StreamingSite {
         }
     }
 
-    private static void changeCreditcard(Scanner scanner, List<Person> tempList) {
-        System.out.println("Pls enter your Old CreditCard Information if you wish to change it. Enter it like :XXXX XXXX XXXX XXXX");
-        String creditcard = scanner.nextLine();
+    private static void changeCreditcard(List<Person> tempList) {
+        String creditcard = InputIn.nextLineOut("Pls enter your Old CreditCard Information if you wish to change it. Enter it like :XXXX XXXX XXXX XXXX");
         for (Person credit : tempList) {
             if (credit.getCreditCard().equals(creditcard)) {
                 System.out.println("Type in your new Creditcard Information");
-                creditcard = scanner.nextLine();
-                creditcard = getCreditcardNumber(scanner, creditcard);
+                creditcard = InputIn.nextLine();
+                creditcard = getCreditcardNumber(creditcard);
                 credit.setCreditCard(creditcard);
             }
         }
@@ -278,16 +263,13 @@ public class StreamingSite {
 
     public static void nameSearch() {
         movies();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Type in the movie name or a word that might appear in its name (if you want to see all Movies just press enter)");
-        String movieFinder = scanner.nextLine();
+        String movieFinder = InputIn.nextLineOut("Type in the movie name or a word that might appear in its name (if you want to see all Movies just press enter)");
         for (Movies movieList : movies()) {
             if (movieList.getName().toLowerCase().contains(movieFinder)) {
                 System.out.println(movieList.getName());
             }
         }
-        System.out.println("Which of these Movies do you want the Information of");
-        String getInformation = scanner.nextLine();
+        String getInformation = InputIn.nextLineOut("Which of these Movies do you want the Information of");
         for (Movies movieList : movies()) {
             if (movieList.getName().equalsIgnoreCase(getInformation)) {
                 System.out.println(movieList.getDuration() + " " + movieList.getGenre() + " " + movieList.getProducer() + " " + movieList.getViews() + " " + movieList.isViewed());
@@ -297,16 +279,13 @@ public class StreamingSite {
 
     public static void genreSearch() {
         movies();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Type in the Genre of the Movie you want to find. Keep in mind the Genre might not fit any movie");
-        String genreFinder = scanner.nextLine();
+        String genreFinder = InputIn.nextLineOut("Type in the Genre of the Movie you want to find. Keep in mind the Genre might not fit any movie");
         for (Movies movieList : movies()) {
             if (movieList.getGenre().toLowerCase().contains(genreFinder)) {
                 System.out.println(movieList.getName());
             }
         }
-        System.out.println("Which of these Movies do you want the Information of");
-        String getInformation = scanner.nextLine();
+        String getInformation = InputIn.nextLineOut("Which of these Movies do you want the Information of");
         for (Movies movieList : movies()) {
             if (movieList.getName().equalsIgnoreCase(getInformation)) {
                 System.out.println(movieList.getDuration() + " " + movieList.getGenre() + " " + movieList.getProducer() + " " + movieList.getViews() + " " + movieList.isViewed());
