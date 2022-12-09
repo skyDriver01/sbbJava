fa88ad928f7e42ce2f4504c382f6b5ba61fe0e80
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-12-09 14:20:01 +0100

    it gives me the correct number back now and accepts multiple words after each other but, it prints all the words out not just the Nouns so only fix that then make it so all words get printed out in the reverse direction like from book, house, fire to fire, house, book

--- src/objektOriented/aufg1/ListsBasics/aufg2.java
+++ src/objektOriented/aufg1/ListsBasics/aufg2.java
@@ -8,9 +8,30 @@ import java.util.List;
 public class aufg2 {
     public static void main(String[] args) {
         List<String> words = new ArrayList<>();
-        String input = InputIn.nextLineOut("Type some words, some can be verbs or nouns they don't have to match to be a sentence anything works.");
-        words.add(input);
+        String getWords;
+        String input;
+        /*do {
+            input = getWords;
+            words.add(input);
+        } while (!(input.equals("exit")));*/
+        getWords = InputIn.nextLineOut(
+                "Type some words, some can be verbs or nouns they don't have to match to be a sentence," +
+                " anything works.(Just be carefull bc when you type:" +
+                " (exit) without the parantheses you will stop being able to type more words)");
+        while (!(input = InputIn.nextLine()).equals("exit")) { //nicht unbedingt so machen bei richtigen code.
+            getWords = (input);
+            words.add(getWords);
+        }
         System.out.println(words.size());
-        System.out.println(words.contains("A-Z"));
+        checkForNouns(words);
+    }
+
+    private static void checkForNouns(List<String> words) {
+        for (String noun : words) {
+            if(noun.matches("^[A-Z]*" +
+                            "[a-zA-Z]*$")) { //Die erste viereck klammer ist dafür da das es für den anfangsbuchstaben schaut das er gross ist und die zweite das der Rest hinten dran entweder klein oder gross ist.
+                System.out.println(noun.toUpperCase());                                                     //Das ^ Ist für anfangsbuchstabe und $ für letzten buchstaben das * zum wiederholen
+            }
+        }
     }
 }
