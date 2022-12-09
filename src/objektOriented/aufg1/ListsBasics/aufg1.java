7bce51ce22826713f3d392cd345806e7083eec61
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-12-09 08:22:17 +0100

    it can get the number at the input position now

--- src/objektOriented/aufg1/ListsBasics/aufg1.java
+++ src/objektOriented/aufg1/ListsBasics/aufg1.java
@@ -7,43 +7,49 @@ import java.util.List;
 
 public class aufg1 {
     static List<Integer> numberList;
+
     public static void main(String[] args) {
-        getNumbersForList ();
-        System.out.println (numberList);
-
+        getNumbersForList();
     }
-    public static void getNumbersForList(){
-        int x, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10;
-        x = InputIn.nextIntOut ("Type a sequence of random Numbers");
-        x1 = InputIn.nextInt ();
-        x2 = InputIn.nextInt ();
-        x3 = InputIn.nextInt ();
-        x4 = InputIn.nextInt ();
-        x5 = InputIn.nextInt ();
-        x6 = InputIn.nextInt ();
-        x7 = InputIn.nextInt ();
-        x8 = InputIn.nextInt ();
-        x9 = InputIn.nextIntOut ("Enter the last number");
 
-        addToList (x, x1, x2, x3, x4, x5, x6, x7, x8, x9);
+    public static void getNumbersForList() {
+        int x, x1, x2, x3, x4, x5, x6, x7, x8, x9;
+        x = InputIn.nextIntOut("Type a sequence of random Numbers");
+        x1 = InputIn.nextInt();
+        x2 = InputIn.nextInt();
+        x3 = InputIn.nextInt();
+        x4 = InputIn.nextInt();
+        x5 = InputIn.nextInt();
+        x6 = InputIn.nextInt();            //TODO make it so you can choose how many numbers you want in the List
+        x7 = InputIn.nextInt();
+        x8 = InputIn.nextInt();
+        x9 = InputIn.nextIntOut("Enter the last number");
+
+        addToList(x, x1, x2, x3, x4, x5, x6, x7, x8, x9);
     }
 
     private static void addToList(int x, int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, int x9) {
-        List<Integer> inputIsPostionInList = new ArrayList<> (); //variabel heisst wenn 3 eingegeben als input das die 3te stelle in der Liste angeschaut wird.
-        inputIsPostionInList.add (x);
-        inputIsPostionInList.add (x1);
-        inputIsPostionInList.add (x2);
-        inputIsPostionInList.add (x3);
-        inputIsPostionInList.add (x4);
-        inputIsPostionInList.add (x5);
-        inputIsPostionInList.add (x6);
-        inputIsPostionInList.add (x7);
-        inputIsPostionInList.add (x8);
-        inputIsPostionInList.add (x9);
+        List<Integer> inputIsPostionInList = new ArrayList<>(); //variabel heisst wenn 3 eingegeben als input das die 3te stelle in der Liste angeschaut wird.
+        inputIsPostionInList.add(x);
+        inputIsPostionInList.add(x1);
+        inputIsPostionInList.add(x2);
+        inputIsPostionInList.add(x3);
+        inputIsPostionInList.add(x4);
+        inputIsPostionInList.add(x5);
+        inputIsPostionInList.add(x6);
+        inputIsPostionInList.add(x7);
+        inputIsPostionInList.add(x8);
+        inputIsPostionInList.add(x9);
         numberList = inputIsPostionInList;
-    //---------------------------------------extract the method when its written fully
-        for (int i = 0; i <= x ; i++) {//TODO rn it only counts like a counter change to make it check the index number ad give that out
-            System.out.println (i);
+        checkNumbPosition(x, inputIsPostionInList);
+    }
+
+    private static void checkNumbPosition(int x, List<Integer> inputIsPostionInList) {
+        if (x <= inputIsPostionInList.size() - 1){
+            System.out.println("This is the number at the position " + x + " that you entered first= " +
+                    inputIsPostionInList.get(x));
+        } else {
+            System.out.println("ERROR! The position " + x + " doesn't exist");
         }
     }
 }
