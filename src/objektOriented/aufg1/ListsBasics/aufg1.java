97fbba3e8b09d37f32c1d5d6b3593807b37710f2
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-12-09 10:42:56 +0100

    accepts both values and makes it so unlimited amount of numbers can be used

--- src/objektOriented/aufg1/ListsBasics/aufg1.java
+++ src/objektOriented/aufg1/ListsBasics/aufg1.java
@@ -10,44 +10,31 @@ public class aufg1 {
 
     public static void main(String[] args) {
         getNumbersForList();
+        System.out.println(numberList);
     }
 
     public static void getNumbersForList() {
-        int x, x1, x2, x3, x4, x5, x6, x7, x8, x9;
-        x = InputIn.nextIntOut("Type a sequence of random Numbers");
-        x1 = InputIn.nextInt();
-        x2 = InputIn.nextInt();
-        x3 = InputIn.nextInt();
-        x4 = InputIn.nextInt();
-        x5 = InputIn.nextInt();
-        x6 = InputIn.nextInt();            //TODO make it so you can choose how many numbers you want in the List
-        x7 = InputIn.nextInt();
-        x8 = InputIn.nextInt();
-        x9 = InputIn.nextIntOut("Enter the last number");
+        List<Integer> inputIsPostionInList = new ArrayList<>();
+        String input;
+        int x, x1;
+        x = Integer.parseInt(InputIn.nextLineOut("Type a sequence of random Numbers if you want to stop adding numbers type: exit"));         //x is the number to search for in the list x1-x9 are the numbers that will be put in the list but after you can choose the amount it would be x1-xUnlimited
+        while (!(input = InputIn.nextLine()).equals("exit")) { //nicht unbedingt so machen bei richtigen code.
+            x1 = Integer.parseInt(input);
+            inputIsPostionInList.add(x1);
+        }
+        //TODO make it so you can type as many numbers as you want until you stay stop
+        inputIsPostionInList.add(x);                //variabel heisst wenn 3 eingegeben als input das die 3te stelle in der Liste angeschaut wird.
 
-        addToList(x, x1, x2, x3, x4, x5, x6, x7, x8, x9);
-    }
-
-    private static void addToList(int x, int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, int x9) {
-        List<Integer> inputIsPostionInList = new ArrayList<>(); //variabel heisst wenn 3 eingegeben als input das die 3te stelle in der Liste angeschaut wird.
-        inputIsPostionInList.add(x);
-        inputIsPostionInList.add(x1);
-        inputIsPostionInList.add(x2);
-        inputIsPostionInList.add(x3);
-        inputIsPostionInList.add(x4);
-        inputIsPostionInList.add(x5);
-        inputIsPostionInList.add(x6);
-        inputIsPostionInList.add(x7);
-        inputIsPostionInList.add(x8);
-        inputIsPostionInList.add(x9);
         numberList = inputIsPostionInList;
+
         checkNumbPosition(x, inputIsPostionInList);
+
     }
 
     private static void checkNumbPosition(int x, List<Integer> inputIsPostionInList) {
         if (x <= inputIsPostionInList.size() - 1){
-            System.out.println("This is the number at the position " + x + " that you entered first= " +
-                    inputIsPostionInList.get(x));
+            System.out.println(
+                    "This is the number at the position " + x + " that you entered = " + inputIsPostionInList.get(x));
         } else {
             System.out.println("ERROR! The position " + x + " doesn't exist");
         }
