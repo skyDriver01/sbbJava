6721a755f985989c3f60dac8dde33e2b2a648ba9
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-12-15 13:24:47 +0100

    going to ask a coach bc i cant understand this problem

--- src/objektOriented/aufg1/Generics/aufg2/NumberList.java
+++ src/objektOriented/aufg1/Generics/aufg2/NumberList.java
@@ -6,25 +6,41 @@ import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
 public class NumberList<T> {
+    List <T> numberList = new ArrayList <>();
 
     public void lists() {
-        List <T> numberList = new ArrayList <>();
-
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i <= 1; i++) {
             IntStream ints = numberList
                     .stream()
-                    .mapToInt(n -> (int) (Math.random() * 100));
+                    .mapToInt(n -> (int) (Math.random() * 100));  // * 100 weil es die zahlen rundet nachdem es randoms findet und ohne das *100 sind alle zahlen einfach 0
             DoubleStream doubles = numberList
                     .stream()
                     .mapToDouble(n -> Math.random() * (1 - 100) + 100);
-            numberList.add((T) ints.boxed().toList());
-            numberList.add((T) doubles.boxed().toList());
-            //* 100 weil es die zahlen rundet nachdem es randoms findet und ohne das *100 sind alle zahlen einfach 0
+            numberList.add((T) ints
+                    .boxed()
+                    .toList());
+            numberList.add((T) doubles
+                    .boxed()
+                    .toList());
+
         }
         System.out.println(numberList);
     }
 
-    public static void getBiggestAndSmallestNumber() {
-
+    public void getBiggestAndSmallestNumber() {
     }
 }
+/*
+            IntStream ints = numberList
+                    .stream()
+                    .mapToInt(n -> (int) (Math.random() * 100));  // * 100 weil es die zahlen rundet nachdem es randoms findet und ohne das *100 sind alle zahlen einfach 0
+            DoubleStream doubles = numberList
+                    .stream()
+                    .mapToDouble(n -> Math.random() * (1 - 100) + 100);
+            numberList.add((T) ints
+                    .boxed()
+                    .toList());
+            numberList.add((T) doubles
+                    .boxed()
+                    .toList());
+ */
