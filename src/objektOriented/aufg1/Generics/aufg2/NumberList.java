861410dd5b79634a508f6b5ce6200bbdf86f46d4
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-12-15 14:01:57 +0100

    sam helped me try it another way now ig gives back the biggest int and smallest int usw.

--- src/objektOriented/aufg1/Generics/aufg2/NumberList.java
+++ src/objektOriented/aufg1/Generics/aufg2/NumberList.java
@@ -2,45 +2,50 @@ package objektOriented.aufg1.Generics.aufg2;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
 
-public class NumberList<T> {
-    List <T> numberList = new ArrayList <>();
+public class NumberList<T extends Comparable> {
+
+    private List <T> list = new ArrayList <>();
+
+    public void addElements(T... elements) {
+        list.addAll(List.of(elements));
+    }
+
+    public T getBiggest() {
+        return list
+                .stream()
+                .max((a, b) -> a.compareTo(b))
+                .orElse(null);
+
+    }
+
+    public T getSmallest() {
+        return list
+                .stream()
+                .min((a, b) -> a.compareTo(b))
+                .orElse(null);
+    }
+
 
     public void lists() {
-        for (int i = 0; i <= 1; i++) {
-            IntStream ints = numberList
-                    .stream()
-                    .mapToInt(n -> (int) (Math.random() * 100));  // * 100 weil es die zahlen rundet nachdem es randoms findet und ohne das *100 sind alle zahlen einfach 0
-            DoubleStream doubles = numberList
-                    .stream()
-                    .mapToDouble(n -> Math.random() * (1 - 100) + 100);
-            numberList.add((T) ints
-                    .boxed()
-                    .toList());
-            numberList.add((T) doubles
-                    .boxed()
-                    .toList());
+        Integer[] ints = new Integer[]{1, 1343, 4, 5345, 6, 7, 8, 54, 3, 3, 2, 1, 67, 57, 4, 5, 74, 3};
+        List <Integer> numberList = List.of(ints);
+        NumberList <Integer> intList = new NumberList <>();
+        intList.addElements(numberList
+                                    .stream()
+                                    .toArray(Integer[]::new));
+        int biggest = intList.getBiggest();
+        int smallest = intList.getSmallest();
+        System.out.println("Biggest Int: " + biggest);
+        System.out.println("Smallest Int: " + smallest);
+        //--------------------------------------------------------
 
-        }
+        Double[] doubles = new Double[]{1.4, 123.5, 3612.451543212415681516845135, 123.54, 534.62, 546.561561241, 2.3, 4.34, 2.8, 6.8, 86.7, 3.4, 2.57, 343.534, 537.66, 3989797.7, 47.789, 6.57};
+        List <Double> doubleNumberList = List.of(doubles);
+        NumberList <Double> doubleList = new NumberList <>();
+        doubleList.addElements(doubleNumberList
+                                       .stream()
+                                       .toArray(Double[]::new));
         System.out.println(numberList);
     }
-
-    public void getBiggestAndSmallestNumber() {
-    }
 }
-/*
-            IntStream ints = numberList
-                    .stream()
-                    .mapToInt(n -> (int) (Math.random() * 100));  // * 100 weil es die zahlen rundet nachdem es randoms findet und ohne das *100 sind alle zahlen einfach 0
-            DoubleStream doubles = numberList
-                    .stream()
-                    .mapToDouble(n -> Math.random() * (1 - 100) + 100);
-            numberList.add((T) ints
-                    .boxed()
-                    .toList());
-            numberList.add((T) doubles
-                    .boxed()
-                    .toList());
- */
