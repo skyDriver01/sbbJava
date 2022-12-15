fca16bb1c374fa94b5576b4236049906a0f01a42
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-12-15 11:53:38 +0100

    gonna go to lunch found a way to make 1 list of both doubles and int just that they get separted to their own list in the main list and the first list of the int is always empty ask coach for help with the stream and how you should procide

--- src/objektOriented/aufg1/Generics/aufg2/NumberList.java
+++ src/objektOriented/aufg1/Generics/aufg2/NumberList.java
@@ -2,29 +2,29 @@ package objektOriented.aufg1.Generics.aufg2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 
 public class NumberList<T> {
-    static List <Double> doubleList = new ArrayList <>();
-    static List <Integer> intList = new ArrayList <>();
 
-    public static <T> void lists(T[] element) {
-        addToIntList();
-        for (int i = 0; i < 8; i++) {
-            doubleList.add(Math.random() * (1 - 100) + 100);
+    public void lists() {
+        List <T> numberList = new ArrayList <>();
+
+        for (int i = 0; i < 2; i++) {
+            IntStream ints = numberList
+                    .stream()
+                    .mapToInt(n -> (int) (Math.random() * 100));
+            DoubleStream doubles = numberList
+                    .stream()
+                    .mapToDouble(n -> Math.random() * (1 - 100) + 100);
+            numberList.add((T) ints.boxed().toList());
+            numberList.add((T) doubles.boxed().toList());
+            //* 100 weil es die zahlen rundet nachdem es randoms findet und ohne das *100 sind alle zahlen einfach 0
         }
-
-        System.out.println(doubleList.toString());
-        System.out.println(intList.toString());
+        System.out.println(numberList);
     }
 
-    private static void addToIntList() {
-        intList.add(23);
-        intList.add(50);
-        intList.add(1);
-        intList.add(87);
-        intList.add(62);
-        intList.add(78);
-        intList.add(90);
-        intList.add(18);
+    public static void getBiggestAndSmallestNumber() {
+
     }
 }
--- src/objektOriented/aufg1/Generics/aufg2/aufg2.java
+++ src/objektOriented/aufg1/Generics/aufg2/aufg2.java
@@ -2,6 +2,7 @@ package objektOriented.aufg1.Generics.aufg2;
 
 public class aufg2 {
     public static void main(String[] args) {
-        NumberList.lists(null);
+        NumberList numberList = new NumberList();
+        numberList.lists();
     }
 }
