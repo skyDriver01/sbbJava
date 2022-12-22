40b27069e72514861db77122e6cdb6fd625d831f
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2022-12-22 13:36:06 +0100

    made it so it gives out the respective shelf number of the product type soon to go hom

--- src/objektOriented/CØØP/CØØPLists.java
+++ src/objektOriented/CØØP/CØØPLists.java
@@ -22,41 +22,41 @@ public class CØØPLists {
 
     public List vegetableList() {
         List <Vegetables> vegetables = new ArrayList <>();
-        vegetables.add(new Vegetables("Cucumber", 0.85, true));
-        vegetables.add(new Vegetables("Tomato", 0.80, true));
+        vegetables.add(new Vegetables("Potato", 0.40, true));
+        vegetables.add(new Vegetables("Onion", 0.65, true));
         vegetables.add(new Vegetables("Carrot", 0.75, true));
-        vegetables.add(new Vegetables("Salad", 2.10, true));
-        vegetables.add(new Vegetables("Potato", 0.4, true));
+        vegetables.add(new Vegetables("Tomato", 0.80, true));
+        vegetables.add(new Vegetables("Cucumber", 0.85, true));
         vegetables.add(new Vegetables("Radish", 1, true));
         vegetables.add(new Vegetables("Beet", 1.10, true));
-        vegetables.add(new Vegetables("Onion", 0.65, true));
         vegetables.add(new Vegetables("Broccoli", 1.15, true));
+        vegetables.add(new Vegetables("Salad", 2.10, true));
         vegetablesList = vegetables;
         return vegetables;
     }
 
     public List fruitList() {
         List <Fruits> fruits = new ArrayList <>();
-        fruits.add(new Fruits("Apple", 1.20, true));
         fruits.add(new Fruits("Pear", 0.95, true));
-        fruits.add(new Fruits("Pineapple", 3, true));
-        fruits.add(new Fruits("Strawberry", 1.50, true));
-        fruits.add(new Fruits("Mango", 1.30, true));
-        fruits.add(new Fruits("Kiwi", 1.20, true));
+        fruits.add(new Fruits("Apple", 1.20, true));
         fruits.add(new Fruits("Papaya", 1.20, true));
+        fruits.add(new Fruits("Kiwi", 1.20, true));
+        fruits.add(new Fruits("Mango", 1.30, true));
+        fruits.add(new Fruits("Strawberry", 1.50, true));
         fruits.add(new Fruits("Pomegranate", 2.50, true));
+        fruits.add(new Fruits("Pineapple", 3, true));
         fruitsList = fruits;
         return fruits;
     }
 
     public List sweetsList() {
         List <Sweets> sweets = new ArrayList <>();
-        sweets.add(new Sweets("Kinder Bueno", 2.10, true));
+        sweets.add(new Sweets("Caramels", 0.50, true));
+        sweets.add(new Sweets("Pop Rocks", 1.40, true));
         sweets.add(new Sweets("Chocolate", 1.50, true));
         sweets.add(new Sweets("Salted Chips", 1.75, true));
-        sweets.add(new Sweets("Caramels", 0.50, true));
         sweets.add(new Sweets("Gummy Worms", 1.80, true));
-        sweets.add(new Sweets("Pop Rocks", 1.40, true));
+        sweets.add(new Sweets("Kinder Bueno", 2.10, true));
         sweets.add(new Sweets("Kinder Choco Bons", 2.50, true));
         sweets.add(new Sweets("Maltesers", 4.10, true));
         sweetsList = sweets;
@@ -65,12 +65,12 @@ public class CØØPLists {
 
     public List domesticApplianceList() {
         List <DomesticAppliances> domesticAppliances = new ArrayList <>();
-        domesticAppliances.add(new DomesticAppliances("Knife", 7.50, true));
-        domesticAppliances.add(new DomesticAppliances("Frying Pan", 15, true));
         domesticAppliances.add(new DomesticAppliances("Lighter", 4, true));
         domesticAppliances.add(new DomesticAppliances("Pot", 5.65, true));
-        domesticAppliances.add(new DomesticAppliances("Dishes", 12.50, true));
+        domesticAppliances.add(new DomesticAppliances("Knife", 7.50, true));
         domesticAppliances.add(new DomesticAppliances("Spatula", 8, true));
+        domesticAppliances.add(new DomesticAppliances("Dishes", 12.50, true));
+        domesticAppliances.add(new DomesticAppliances("Frying Pan", 15, true));
         domesticAppliances.add(new DomesticAppliances("Boiler", 20.50, true));
         domesticAppliancesList = domesticAppliances;
         return domesticAppliances;
@@ -78,10 +78,10 @@ public class CØØPLists {
 
     public List buildingMaterialsList() {
         List <BuildingMaterial> buildingMaterials = new ArrayList <>();
+        buildingMaterials.add(new BuildingMaterial("Leather", 5.75, true));
         buildingMaterials.add(new BuildingMaterial("Wood", 10, true));
         buildingMaterials.add(new BuildingMaterial("Brick", 20, true));
         buildingMaterials.add(new BuildingMaterial("Iron", 30.60, true));
-        buildingMaterials.add(new BuildingMaterial("Leather", 5.75, true));
         buildingMaterials.add(new BuildingMaterial("Diamond", 50000, true));
         buildingMaterials.add(new BuildingMaterial("Gold", 10000, true));
         buildingMaterials.add(new BuildingMaterial("Netherite", 100000, true));
@@ -91,29 +91,35 @@ public class CØØPLists {
 
     public List <Shelves> shelvesList() {
         List <Shelves> shelves = new ArrayList <>();
-        shelves.add(new Shelves(0, "Drinks", ""));
-        shelves.add(new Shelves(1, "Fruits", ""));
-        shelves.add(new Shelves(2, "Vegetables", ""));
-        shelves.add(new Shelves(3, "Domestic Appliances", ""));
-        shelves.add(new Shelves(4, "Sweets", ""));
-        shelves.add(new Shelves(5, "Pastry", ""));
-        shelves.add(new Shelves(6, "Hygiene", ""));
+        shelves.add(new Shelves(0, "Drinks", "Wangen an der Aare"));
+        shelves.add(new Shelves(1, "Fruits", "Wangen an der Aare"));
+        shelves.add(new Shelves(2, "Vegetables", "Wangen an der Aare"));
+        shelves.add(new Shelves(4, "Sweets", "Wangen an der Aare"));
+        shelves.add(new Shelves(5, "Pastry", "Wangen an der Aare"));
+        shelves.add(new Shelves(21, "Building Materials", "Wangen an der Aare"));
 
-        shelves.add(new Shelves(7, "Drinks", ""));
-        shelves.add(new Shelves(8, "Fruits", ""));
-        shelves.add(new Shelves(9, "Vegetables", ""));
-        shelves.add(new Shelves(10, "Domestic Appliances", ""));
-        shelves.add(new Shelves(11, "Sweets", ""));
-        shelves.add(new Shelves(12, "Pastry", ""));
-        shelves.add(new Shelves(13, "Hygiene", ""));
+        shelves.add(new Shelves(3, "Domestic Appliances", "Bern Bahnhof"));
+        shelves.add(new Shelves(6, "Hygiene", "Bern Bahnhof"));
+        shelves.add(new Shelves(7, "Drinks", "Bern Bahnhof"));
+        shelves.add(new Shelves(9, "Vegetables", "Bern Bahnhof"));
+        shelves.add(new Shelves(12, "Pastry", "Bern Bahnhof"));
+        shelves.add(new Shelves(23, "Building Materials", "Bern Bahnhof"));
 
-        shelves.add(new Shelves(14, "Drinks", ""));
-        shelves.add(new Shelves(15, "Fruits", ""));       //  TODO: Make more Shelves for all the Locations and also not all locations can have all the available items!
-        shelves.add(new Shelves(16, "Vegetables", ""));
-        shelves.add(new Shelves(17, "Domestic Appliances", ""));
-        shelves.add(new Shelves(18, "Sweets", ""));
-        shelves.add(new Shelves(19, "Pastry", ""));
-        shelves.add(new Shelves(20, "Hygiene", ""));
+        shelves.add(new Shelves(8, "Fruits", "Zürichsee"));
+        shelves.add(new Shelves(11, "Sweets", "Zürichsee"));
+        shelves.add(new Shelves(13, "Hygiene", "Zürichsee"));
+        shelves.add(new Shelves(14, "Drinks", "Zürichsee"));
+
+        shelves.add(new Shelves(10, "Domestic Appliances", "Grosse Schanze"));
+        shelves.add(new Shelves(15, "Fruits", "Grosse Schanze"));
+        shelves.add(new Shelves(16, "Vegetables", "Grosse Schanze"));
+        shelves.add(new Shelves(18, "Sweets", "Grosse Schanze"));
+
+        shelves.add(new Shelves(17, "Domestic Appliances", "Zürich Bahnhof"));
+        shelves.add(new Shelves(19, "Pastry", "Zürich Bahnhof"));
+        shelves.add(new Shelves(20, "Hygiene", "Zürich Bahnhof"));
+        shelves.add(new Shelves(22, "Building Materials", "Zürich Bahnhof"));
+
         shelvesList = shelves;
         return shelves;
 
@@ -123,32 +129,32 @@ public class CØØPLists {
         List <Stores> stores = new ArrayList <>();
         stores.add(new Stores("Bern", "Wangen an der Aare"));
         stores.add(new Stores("Bern", "Bern Bahnhof"));
+        stores.add(new Stores("Bern", "Grosse Schanze"));
         stores.add(new Stores("Zürich", "Zürich Bahnhof"));
         stores.add(new Stores("Zürich", "Zürichsee"));
-        stores.add(new Stores("Bern", "Grosse Schanze"));
         storesList = stores;
         return stores;
     }
 
     public List pastryList() {
         List <Pastry> pastries = new ArrayList <>();
-        pastries.add(new Pastry("Bread", 0, ""));
-        pastries.add(new Pastry("Croissant", 0, ""));
-        pastries.add(new Pastry("Weggli", 0, ""));
-        pastries.add(new Pastry("Donut", 0, ""));
-        pastries.add(new Pastry("Bretzel", 0, ""));
+        pastries.add(new Pastry("Bread", 0.80, "A123"));
+        pastries.add(new Pastry("Croissant", 1.20, "A234"));
+        pastries.add(new Pastry("Weggli", 1, "A345"));
+        pastries.add(new Pastry("Donut", 2.10, "A456"));
+        pastries.add(new Pastry("Bretzel", 2.40, "A567"));
         pastryList = pastries;
         return pastries;
     }
 
-    public List hygieneList() {        //Hygiene products like face wash or toothbrush usw.
+    public List hygieneList() {
         List <Hygiene> hygienes = new ArrayList <>();
-        hygienes.add(new Hygiene("Toothbrush", 7.45, true));
-        hygienes.add(new Hygiene("Face Cream", 5.35, true));
-        hygienes.add(new Hygiene("Hand Cream", 3.20, true));
         hygienes.add(new Hygiene("Labello", 2.15, true));
-        hygienes.add(new Hygiene("Axe Deodorant", 4.25, true));
+        hygienes.add(new Hygiene("Hand Cream", 3.20, true));
         hygienes.add(new Hygiene("Floss", 3.50, true));
+        hygienes.add(new Hygiene("Axe Deodorant", 4.25, true));
+        hygienes.add(new Hygiene("Face Cream", 5.35, true));
+        hygienes.add(new Hygiene("Toothbrush", 7.45, true));
         hygieneList = hygienes;
         return hygienes;
     }
@@ -189,13 +195,13 @@ public class CØØPLists {
 
     public List drinkList() {
         List <Drinks> drinks = new ArrayList <>();
-        drinks.add(new Drinks("Redbull", 2.35, true));
-        drinks.add(new Drinks("Fanta", 1.50, true));
-        drinks.add(new Drinks("Cola", 1.75, true));
-        drinks.add(new Drinks("Sprite", 1.60, true));
         drinks.add(new Drinks("Ice Tea", 0.80, true));
-        drinks.add(new Drinks("Pepsi", 3, true));
+        drinks.add(new Drinks("Fanta", 1.50, true));
+        drinks.add(new Drinks("Redbull", 2.35, true));
+        drinks.add(new Drinks("Sprite", 1.60, true));
+        drinks.add(new Drinks("Cola", 1.75, true));
         drinks.add(new Drinks("Schorle", 2.10, true));
+        drinks.add(new Drinks("Pepsi", 3, true));
         drinksList = drinks;
         return drinks;
     }
--- src/objektOriented/CØØP/CØØPTablets.java
+++ src/objektOriented/CØØP/CØØPTablets.java
@@ -6,21 +6,104 @@ public class CØØPTablets {
 
     public void access() {
         getAccessToLists();
-        System.out.println("Welcome to CØØP. Here is a list of all our Firms Locations so you shall find what you desire.");
-        CØØPLists.storesList
-                .stream()
-                .forEach(a -> System.out.println(
-                        "The Stores Address: " + a.getAddress() + ", The Kanton its in: " + a.getKanton()));
-        System.out.println("-----------------------------------------------------------------------------------");
-        String chooseAStore = InputIn.nextLineOut("What Store would you like to look over? Type in only to the Stores Address.");
-        switch (chooseAStore){
-            case "Wangen an der Aare" -> {
-                System.out.println("What would you like to buy in this Store?");
+        String getItemClass = InputIn.nextLineOut("Welcome to CØØP. What kind of Item are you searching for? These are the Item Options: (Vegetables, Fruits, Sweets, Drinks, Hygiene, Pastries, Domestic Appliances, Building Materials)");
+        switch (getItemClass) {
+            case "Vegetables" -> {
+                CØØPLists.shelvesList
+                        .stream()
+                        .filter(a -> a
+                                .getProducts()
+                                .equals(getItemClass))
+                        .forEach(a -> System.out.println(
+                                "These are the Shelf Numbers of the Product Type you are Looking for: " +
+                                a.getShelfNumber() + " and the Address of the Store from that Shelf is: " +
+                                a.getAddress()));
+                String getVeggies = InputIn.nextLineOut("Type a veggie or anything else and we will tell you if that is available and in what stores.");
+                CØØPLists.vegetablesList
+                        .stream()
+                        .filter(a -> a
+                                .getProduct()
+                                .equals(getVeggies))
+                        .forEach(a -> System.out.println(a.getPrice()));
             }
-            case "Bern Bahnhof" -> {}
-            case "Zürich Bahnhof" -> {}
-            case "Zürichsee" -> {}
-            case "Grosse Schanze" -> {}
+            case "Fruits" -> {
+                CØØPLists.shelvesList
+                        .stream()
+                        .filter(a -> a
+                                .getProducts()
+                                .equals(getItemClass))
+                        .forEach(a -> System.out.println(
+                                "These are the Shelf Numbers of the Product Type you are Looking for: " +
+                                a.getShelfNumber() + " and the Address of the Store from that Shelf is: " +
+                                a.getAddress()));
+            }
+            case "Sweets" -> {
+                CØØPLists.shelvesList
+                        .stream()
+                        .filter(a -> a
+                                .getProducts()
+                                .equals(getItemClass))
+                        .forEach(a -> System.out.println(
+                                "These are the Shelf Numbers of the Product Type you are Looking for: " +
+                                a.getShelfNumber() + " and the Address of the Store from that Shelf is: " +
+                                a.getAddress()));
+            }
+            case "Drinks" -> {
+                CØØPLists.shelvesList
+                        .stream()
+                        .filter(a -> a
+                                .getProducts()
+                                .equals(getItemClass))
+                        .forEach(a -> System.out.println(
+                                "These are the Shelf Numbers of the Product Type you are Looking for: " +
+                                a.getShelfNumber() + " and the Address of the Store from that Shelf is: " +
+                                a.getAddress()));
+            }
+            case "Hygiene" -> {
+                CØØPLists.shelvesList
+                        .stream()
+                        .filter(a -> a
+                                .getProducts()
+                                .equals(getItemClass))
+                        .forEach(a -> System.out.println(
+                                "These are the Shelf Numbers of the Product Type you are Looking for: " +
+                                a.getShelfNumber() + " and the Address of the Store from that Shelf is: " +
+                                a.getAddress()));
+            }
+            case "Pastries" -> {
+                CØØPLists.shelvesList
+                        .stream()
+                        .filter(a -> a
+                                .getProducts()
+                                .equals(getItemClass))
+                        .forEach(a -> System.out.println(
+                                "These are the Shelf Numbers of the Product Type you are Looking for: " +
+                                a.getShelfNumber() + " and the Address of the Store from that Shelf is: " +
+                                a.getAddress()));
+            }
+            case "Domestic Appliances" -> {
+                CØØPLists.shelvesList
+                        .stream()
+                        .filter(a -> a
+                                .getProducts()
+                                .equals(getItemClass))
+                        .forEach(a -> System.out.println(
+                                "These are the Shelf Numbers of the Product Type you are Looking for: " +
+                                a.getShelfNumber() + " and the Address of the Store from that Shelf is: " +
+                                a.getAddress()));
+            }
+            case "Building Materials" -> {
+                CØØPLists.shelvesList
+                        .stream()
+                        .filter(a -> a
+                                .getProducts()
+                                .equals(getItemClass))
+                        .forEach(a -> System.out.println(
+                                "These are the Shelf Numbers of the Product Type you are Looking for: " +
+                                a.getShelfNumber() + " and the Address of the Store from that Shelf is: " +
+                                a.getAddress()));
+            }
+            default -> System.out.println("That Category does not exist");
         }
     }
 
@@ -38,5 +121,24 @@ public class CØØPTablets {
         cøøpLists.schüpercardList();
         cøøpLists.sweetsList();
         cøøpLists.workerList();
+        cøøpLists.customerList();
     }
 }
+/*
+CØØPLists.storesList
+                .stream()
+                .forEach(a -> System.out.println(
+                        "The Stores Address: " + a.getAddress() + ", The Kanton its in: " + a.getKanton()));
+        System.out.println("-----------------------------------------------------------------------------------");
+String chooseAStore = InputIn.nextLineOut("What Store would you like to look over? Type in only to the Stores Address.");
+switch (chooseAStore){
+            case "Wangen an der Aare" -> {
+                System.out.println("What would you like to buy in this Store?");
+
+            }
+            case "Bern Bahnhof" -> {}
+            case "Zürich Bahnhof" -> {}
+            case "Zürichsee" -> {}
+            case "Grosse Schanze" -> {}
+        }
+ */
