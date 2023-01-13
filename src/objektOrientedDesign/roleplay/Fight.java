3c1fe2015d925847692350b3d9aa678cce6553bb
skyDriver01 <116278995+skyDriver01@users.noreply.github.com>
2023-01-13 09:08:49 +0100

    color can be used no

--- src/objektOrientedDesign/roleplay/Fight.java
+++ src/objektOrientedDesign/roleplay/Fight.java
@@ -5,41 +5,59 @@ import objektOrientedDesign.roleplay.gamefigures.Gamefigurine;
 
 import java.util.HashMap;
 
+import static objektOrientedDesign.roleplay.TextStuff.Line;
+import static objektOrientedDesign.roleplay.TextStuff.color;
+
 public class Fight {
+    private static int i = 0;
+
 
     public static void startTheFight(Gamefigurine playerOne, Gamefigurine playerTwo) {
         System.out.println("It seems all preparations are done from both ends time to start the fight lets see who gets to start");
         if(playerOne.getSpeed() > playerTwo.getSpeed()) {
-            fightOptions(playerOne, playerTwo);            // Todo: Find a better way for this. Not for the random speed thats good but for the 2 player to then get chosen who plays after who with the 20 round max rule.
+            fightOptions(playerOne, playerTwo);
         } else if(playerTwo.getSpeed() > playerOne.getSpeed()) {
             fightOptions(playerTwo, playerOne);
         }
-        for (int i = 0; i <= 20 ; i++) {
-            fightOptions(playerOne, playerTwo);
-            fightOptions(playerTwo, playerOne);
-            if(playerOne.getlP() < 0 || playerTwo.getlP() < 0){
-                if(playerOne.getlP() < 0){
-                    System.out.println("Player Two won. So the player who chose : " + playerTwo.getName());
-                    System.out.println(playerTwo.getlP() + " Is the Remaining amount of health on " + playerTwo.getName());
-                    System.out.println(playerOne.getlP() + " has died with that amount " + playerOne.getName());
-
-                } else if(playerTwo.getlP() < 0){
-                    System.out.println("Player One won. So the player who chose : " + playerOne.getName());
-                    System.out.println(playerOne.getlP() + " Is the Remaining amount of health on " + playerOne.getName());
-                    System.out.println(playerTwo.getlP() + " has died with that amount " + playerTwo.getName());
-                }
+        if(i <= 20) {
+            for (i = 0; i <= 20; i++) {
+                fightOptions(playerOne, playerTwo);
+                i = winningConditions(playerOne, playerTwo, i);
+                fightOptions(playerTwo, playerOne);
+                i = winningConditions(playerTwo, playerOne, i);
             }
         }
     }
 
+    private static int winningConditions(Gamefigurine playerOne, Gamefigurine playerTwo, int i) {
+        if(playerOne.getlP() < 0 || playerTwo.getlP() < 0) {
+            if(playerOne.getlP() < 0) {
+                i = 21;
+                System.out.println(Line());
+                System.out.println("So the player who chose : " + playerTwo.getName() + " won");
+                System.out.println(color("yellow") + playerTwo.getlP() + " Is the Remaining amount of health on " + playerTwo.getName() + color("r"));
+                System.out.println(playerOne.getlP() + " has died with that amount on " + playerOne.getName());
+
+            } else if(playerTwo.getlP() < 0) {
+                i = 21;
+                System.out.println("So the player who chose : " + playerOne.getName() + " won");
+                System.out.println(playerOne.getlP() + " Is the Remaining amount of health on " + playerOne.getName());
+                System.out.println(playerTwo.getlP() + " has died with that amount on " + playerTwo.getName());
+            }
+        }
+        return i;
+    }
+
     public static void fightOptions(Gamefigurine player, Gamefigurine opponent) {
         System.out.println("Seems that the player who chose " + player.getName() + " Will Be the one to choose their move");
+
         HashMap <Integer, String> chooseNextMove = new HashMap <>();
         chooseNextMove.put(1, "1: Attack enemy");
         chooseNextMove.put(2, "2: Activate Different Weapon");
-        chooseNextMove.put(3, "3: Use Item");
-        chooseNextMove.put(4, "4: Do Nothing");
-        chooseNextMove.put(5, "5: Give up");
+        chooseNextMove.put(3, "3: Drop Weapon");
+        chooseNextMove.put(4, "4: Use Item");
+        chooseNextMove.put(5, "5: Do Nothing");
+        chooseNextMove.put(6, "6: Give up");
         for (int i = 1; i <= chooseNextMove.size(); i++) {
             System.out.println(chooseNextMove.get(i));
         }
@@ -48,15 +66,24 @@ public class Fight {
             case 1 -> attackOpponent(player, opponent);
 
             case 2 -> {
+                System.out.println("what weapon do you wish to activate?");
+                //TOdo find a list to make a stream or for each
                 //TODO: When making this lookout for the bug where i activate other weapon but the last weapons effects still stay bc that will be broken
             }
             case 3 -> {
+                player.setcC(player.getcC() + player.getWeapon().getWeight());
+                player.setWeapon(null);
+                System.out.println(player.getWeapon().getWeapon());
+            }
+            case 4 -> {
                 //TODO : is supposed to take up the turn
             }
-            case 4 ->
-                    System.out.println("Ok seems understandable?");       //Maybe make small Easter egg that if done 3 times you will get 4xFV
             case 5 ->
-                    System.exit(0);   //When there is the possibility with the winner and the upper to-do is changed this to also say who won due to the enemy giving up
+                    System.out.println("Ok seems understandable?");       //Maybe make small Easter egg that if done 3 times you will get 4xFV
+            case 6 -> {
+                player.setlP(-1);
+                winningConditions(player, opponent, 21); //When there is the possibility with the winner and the upper to-do is changed this to also say who won due to the enemy giving up
+            }
             default -> System.out.println("So just do the same as 4? Got it.");
         }
     }
@@ -66,7 +93,9 @@ public class Fight {
             if(opponent.getArmor() != null) {      //some sort of bug is here that says armor is null find option to get this fixed so yk no damage?
                 //Also look about how to make the instant death thing in here, so it can occur if the player has it
                 //Seems like the problem is when you have no armor that it says it won't work so mhm.
-                if(!(Math.random() * 1 < 0 + opponent.getArmor().getNoDamage())) {
+                if(!(Math.random() * 1 < 0 + opponent
+                        .getArmor()
+                        .getNoDamage())) {
                     opponent.setlP(opponent.getlP() - (player.getfV() / opponent.getRes()));
                     System.out.println(player.getlP() + " Is the Remaining amount of health on " + player.getName());
                     System.out.println(opponent.getlP() + " Is the Remaining amount of health on " + opponent.getName());
---
+++ src/objektOrientedDesign/roleplay/TextStuff.java
@@ -0,0 +1,32 @@
+package objektOrientedDesign.roleplay;
+
+public class TextStuff {
+
+    public static String Line() {
+        return color("white") + ":::::::::::::::::::::::::::::::::::::::::::::::::::::::" + color("r");
+    }
+
+    public static String color(String color) {
+        final String ANSI_BLACK = "\u001B[30m";
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_GREEN = "\u001B[32m";
+        final String ANSI_YELLOW = "\033[1;93m";
+        final String ANSI_BLUE = "\u001B[34m";
+        final String ANSI_PURPLE = "\u001B[35m";
+        final String ANSI_CYAN = "\u001B[36m";
+        final String ANSI_WHITE = "\u001B[37m";
+        final String ANSI_RESET = "\u001B[0m";
+
+        return switch (color) {
+            case "black" -> ANSI_BLACK;
+            case "red" -> ANSI_RED;
+            case "green" -> ANSI_GREEN;
+            case "yellow" -> ANSI_YELLOW;
+            case "blue" -> ANSI_BLUE;
+            case "purple" -> ANSI_PURPLE;
+            case "cyan" -> ANSI_CYAN;
+            case "white" -> ANSI_WHITE;
+            default -> ANSI_RESET;
+        };
+    }
+}
