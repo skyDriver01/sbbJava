0bdfc6e042748d88790eb9a8580ce102c8aa6f84
Nikolic Luka (IT-PTR-CEN2-YPT3 - Extern) <luka.nikolic@sbb.ch>
2023-02-15 08:51:00 +0100

    myset done only maps and more

--- src/generics_two/set/aufg2/MySet.java
+++ src/generics_two/set/aufg2/MySet.java
@@ -1,35 +1,64 @@
 package generics_two.set.aufg2;
 
+import java.util.Arrays;
+
 public class MySet<E> implements MySetInterfaceSimple <Object> {
 
     private Object[] mySet = new Object[0];
     @Override
     public boolean add(Object element) {
-        Object[] secondSet = new Object[mySet.length + 1];
-        System.arraycopy(mySet, 0, secondSet, 0, mySet.length);
+        for (Object obj : mySet) {
+            if (obj == null && element == null) {
+                return false;
+            }
+            if (obj != null && obj.equals(element)) {
+                return false;
+            }
+        }
+
+        E[] secondSet = (E[]) Arrays.copyOf(mySet, mySet.length + 1);
         mySet = secondSet;
         mySet[mySet.length - 1] = element;
-        System.out.println(element + " Is now in your set");
-        return false;
+        return true;
     }
 
     @Override
     public boolean remove(Object object) {
-        Object[] secondArray = new Object[mySet.length + 1];
+        boolean removed = false;
+        Object[] secondSet = new Object[mySet.length - 1];
+        int j = 0;
         for (int i = 0; i < mySet.length; i++) {
-            if(mySet[i] == object) {
-                mySet = secondArray;
-                mySet[mySet.length - 1] = -1;
+            if(mySet[i] == null) {
+                if(object == null) {
+                    removed = true;
+                }
+            } else {
+                if(mySet[i].equals(object)) {
+                    removed = true;
+                    if(i == mySet.length - 1) {
+                        break;
+                    }
+                } else {
+                    secondSet[j++] = mySet[i];
+                }
+            }
+        }
+        if(removed) {
+            if(j > 0) {
+                mySet = secondSet;
+            } else {
+                mySet = new Object[mySet.length -1];
             }
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
     @Override
     public boolean contains(Object object) {
         for (int i = 0; i < mySet.length; i++) {
-            if(mySet[i].equals(object)){
+            if (mySet[i] == (object)) {
                 return true;
             }
         }
@@ -43,18 +72,16 @@ public class MySet<E> implements MySetInterfaceSimple <Object> {
 
     @Override
     public boolean isEmpty() {
-        if(mySet.length == 0){
-            return true;
-        }
-        return false;
+        return mySet.length == 0;
     }
 
     @Override
     public void clear() {
-        mySet = new Object[0];
+        mySet = Arrays.copyOf(mySet, 0);
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         return getClass().getName() + "," + Integer.toHexString(hashCode());
     }
 }
