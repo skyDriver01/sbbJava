e730e8466d5c60d1bf7b03a52ce9f60b771b1e0c
Nikolic Luka (IT-PTR-CEN2-YPT3 - Extern) <luka.nikolic@sbb.ch>
2023-03-09 13:02:44 +0100

    would say that the aufg 5 of reflections is done so it is done aswell now

--- src/reflection/Main.java
+++ src/reflection/Main.java
@@ -1,14 +1,16 @@
 package reflection;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
+
     public static void main(String[] args) {
-        Constructor<?> constructor = null;
-        Class<?> clazz = Person.class;
+        Constructor <?> constructor = null;
+        Class <?> clazz = Person.class;
         try {
             constructor = clazz.getDeclaredConstructor(String.class, int.class);
-        } catch (NoSuchMethodException | NullPointerException | ClassCastException e) {
+        } catch (NoSuchMethodException | ClassCastException e) {
             System.out.println("Smth aint worked");
         }
         assert constructor != null;
@@ -16,6 +18,12 @@ public class Main {
         String[] personNames = personalData.names();
         int[] personAges = personalData.ages();
         for (int i = 0; i < personAges.length; i++) {
+            try {
+                constructor.newInstance(personNames[i], personAges[i]);
+            }catch (InstantiationException | InvocationTargetException | IllegalAccessException e){
+                System.out.println("It wont allow the instance");
+                throw  new RuntimeException(e);
+            }
             System.out.println(personNames[i]);
             System.out.println(personAges[i]);
         }
