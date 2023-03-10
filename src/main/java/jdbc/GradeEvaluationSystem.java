46081cc298a1c4cec735c3acbdc09d4fd27fa104
Nikolic Luka (IT-PTR-CEN2-YPT3 - Extern) <luka.nikolic@sbb.ch>
2023-03-10 15:35:05 +0100

    made the databasses in maria

--- src/main/java/jdbc/GradeEvaluationSystem.java
+++ src/main/java/jdbc/GradeEvaluationSystem.java
@@ -1,15 +1,30 @@
 package jdbc;
 
+import objektOriented.aufg1.aufg2.InputIn;
+
 import java.sql.*;
 
 public class GradeEvaluationSystem {
     static String query = "";
+
     public static void main(String[] args) {
-        captureGrade();
-        deleteGrade();
-        alterGrade();
-        giveOutModuleGrade();
-        transcript();
+        System.out.println("captureGrade");
+        System.out.println("deleteGrade");
+        System.out.println("alterGrade");
+        System.out.println("giveOutModuleGrade");
+        System.out.println("transcript");
+        String choice = InputIn.nextLineOut("What would you like to do: ");
+        switch (choice) {
+            case "captureGrade" -> captureGrade();
+            case "deleteGrade" -> captureGrade();
+            case "alterGrade" -> captureGrade();
+            case "giveOutModuleGrade" -> captureGrade();
+            case "transcript" -> transcript();
+            default -> {
+                System.out.println("pls be careful of the spelling");
+                main(new String[]{});
+            }
+        }
     }
 
     private static void configDatabase() {
@@ -19,25 +34,31 @@ public class GradeEvaluationSystem {
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
             PreparedStatement statement = connection.prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery(query);
+            ResultSet resultSet = statement.executeQuery(); // ExecuteQuery bei Select andere Befehl executeupdate bei create execute bei Insertinto
+            System.out.println(resultSet);
         } catch (SQLException e) {
             System.out.println(e);
         }
     }
 
-    public static void captureGrade(){
+    public static void captureGrade() {
+        query = "SELECT * FROM gradingSystem";
         configDatabase();
     }
-    public static void deleteGrade(){
+
+    public static void deleteGrade() {
         configDatabase();
     }
-    public static void alterGrade(){
+
+    public static void alterGrade() {
         configDatabase();
     }
-    public static void giveOutModuleGrade(){
+
+    public static void giveOutModuleGrade() {
         configDatabase();
     }
-    public static void transcript(){        // Zeugnis auf english
+
+    public static void transcript() {        // Zeugnis auf english
         configDatabase();
     }
 }
